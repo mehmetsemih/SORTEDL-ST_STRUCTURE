5684affb568c20620bbecd1e78de0e1caa3b1729
mehmetsemih <mehmetsemihunal@gmail.com>
2024-07-20 15:24:40 +0300

    sortedlist_structure

--- SORTEDLİST_STRUCTURE/Program.cs
+++ SORTEDLİST_STRUCTURE/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace SORTEDLİST_STRUCTURE
+﻿using System.Collections;
+
+namespace SORTEDLİST_STRUCTURE
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+            //SORTEDLİST STRUCTURE
+            //BELONGS TO SYSTEM.COLLECTION CLASS
+
+            // SORTEDLİST DESCRIBING 
+
+            var list = new SortedList();
 
 
 
